125f27bacb373f432b72be9045658d3fd0ab660c
ArfaGhaffar <91720371+ArfaGhaffar@users.noreply.github.com>
2021-11-06 22:38:46 +0500

    Mainpage layout

---
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+   Button btn ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btn = findViewById(R.id.button1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            openActivity1();
+            }
+        });
+
+    }
+
+    private void openActivity1() {
+        Intent intent = new Intent(this,quiz.class);
+        startActivity(intent);
+    }
+
+
+}
+
+
+
+
+
+
+
+/*Typeface typeface = Typeface.createFromAsset(textView.getText().getAssets(),"src/main/Jameel.ttf");
+        this.textView.setTypeface(typeface);*/
