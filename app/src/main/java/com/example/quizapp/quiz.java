e1b7a80b44c041db94ecaf19dd52db4f8d122a95
ArfaGhaffar <91720371+ArfaGhaffar@users.noreply.github.com>
2021-11-07 02:50:13 +0500

    Adding questions

--- app/src/main/java/com/example/quizapp/quiz.java
+++ app/src/main/java/com/example/quizapp/quiz.java
@@ -12,7 +12,7 @@ import java.util.Random;
 public class quiz extends AppCompatActivity {
 
     TextView textView1,textView2;
-    Button btn,btn2,btn3,bt4;
+    Button btn,btn2,btn3,btn4;
     Random random;
     ArrayList<quizmodel> quizArray;
     int currentScore=0, currentPos, questionAttempted=1;
@@ -21,5 +21,23 @@ public class quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        textView1 = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        btn = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
+        random = new Random();
+        quizArray = new ArrayList<>();
+        getQuizQuestion(quizArray);
+        currentPos= random.nextInt(quizArray.size());
+    }
+
+    private void getQuizQuestion(ArrayList<quizmodel> quizArray) {
+        quizArray.add(new quizmodel("What is the color of apple","Red","blue","yellow","orange","Red"));
+        quizArray.add(new quizmodel("What is the color of orange","Orange","blue","yellow","orange","Orange"));
+        quizArray.add(new quizmodel("What is the color of banana","Yellow","blue","yellow","orange","Yellow"));
+        quizArray.add(new quizmodel("What is the color of leaf","Green","blue","yellow","orange","Green"));
     }
 }
